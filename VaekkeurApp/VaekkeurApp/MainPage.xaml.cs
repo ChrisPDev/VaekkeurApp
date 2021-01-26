2b8216712c239f2e21069fd35b4b8accef5f9e3c
Christoffer Petersen <Christoffer Petersen@ZYLLEPET>
2021-01-26 10:57:04 +0100

    Ny branch med funktionen CheckTimeForMatch

--- VaekkeurApp/VaekkeurApp/MainPage.xaml.cs
+++ VaekkeurApp/VaekkeurApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediaManager;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,31 @@ namespace VaekkeurApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Checks if there is a match between device time and the given time in seconds
+        /// </summary>
+        /// <param name="alarmTimeInSeconds"></param>
+        /// <param name="alarmTone"></param>
+        /// <returns></returns>
+        public async Task CheckTimeForMatch(int alarmTimeInSeconds, string alarmToneUrl)
+        {
+            // Device time now
+            DateTime time = DateTime.Now;
+
+            // Recalculating device time into total seconds
+            int hoursInSeconds = time.Hour * 60 * 60;
+            int minutesInSeconds = time.Minute * 60;
+            int seconds = time.Second;
+            int totalTimeInSeconds = hoursInSeconds + minutesInSeconds + seconds;
+
+            // Check if there is a match between device time and alarm time
+            if (totalTimeInSeconds == alarmTimeInSeconds)
+            {
+                // If match play a sound
+                await CrossMediaManager.Current.Play(alarmToneUrl);
+            }
         }
     }
 }
