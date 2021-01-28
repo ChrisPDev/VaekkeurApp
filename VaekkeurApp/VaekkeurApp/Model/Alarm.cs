f8ab0d9791bdf90dcac7dfcca75c9b6bd636b796
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-01-28 10:12:51 +0100

    Listview with test input
    ListView there test input so we can see what it do

---
+++ VaekkeurApp/VaekkeurApp/Model/Alarm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaekkeurApp.Model
+{
+    class Alarm
+    {
+        public string Title { get; set; }
+        public string Time { get; set; }
+        public bool Switch { get; set; }
+    }
+}
